b918698e96b4502d285edd4b58bbe2b5f8f56e00
Jakab Gabor <jakab.gabor0622@gmail.com>
2023-01-22 13:22:17 +0100

    almost done

--- vehicle-register-starter/src/main/java/hu/ltk/jakabgabor/starter/StarterMain.java
+++ vehicle-register-starter/src/main/java/hu/ltk/jakabgabor/starter/StarterMain.java
@@ -1,7 +1,19 @@
 package hu.ltk.jakabgabor.starter;
 
+import hu.ltk.jakabgabor.controller.VehicleUiController;
+import hu.ltk.jakabgabor.services.VehicleRegister;
+import hu.ltk.jakabgabor.services.VehicleRegisterInteractor;
+import hu.ltk.jakabgabor.presenter.Presenter;
+
+import java.util.Scanner;
+
 public class StarterMain {
     public static void main(String[] args) {
+        Presenter presenter = new Presenter();
+        VehicleRegister vehicleRegister = new VehicleRegister();
+        VehicleRegisterInteractor vehicleRegisterInteractor = new VehicleRegisterInteractor(presenter, vehicleRegister);
+        VehicleUiController vehicleUiController = new VehicleUiController(new Scanner(System.in), vehicleRegisterInteractor);
+        vehicleUiController.run();
 
     }
 }
